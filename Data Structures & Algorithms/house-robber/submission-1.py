51ee951abe0fca668eb6e3e9ead35655f9da88ab
JuSuHo <j5201314peter@gmail.com>
2026-05-10 21:22:44 +0800

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # can rob or not depend on the previous house robbed or not
+        # rob => how much can get
+
+        # dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+
+        if len(nums) == 1:
+            return nums[0]
+            
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        # dp[1] = nums[1]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+
+        return dp.pop()
